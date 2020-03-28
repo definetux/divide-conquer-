3bb04d798b84a4e5371ecaa4e5c98ec6c0486eea
maxim_g <goncharuk.m.n@gmail.com>
2020-03-28 12:12:29 +0200

    Add correct auth response

--- backend/src/main/kotlin/com/binary/app/auth/AuthController.kt
+++ backend/src/main/kotlin/com/binary/app/auth/AuthController.kt
@@ -1,11 +1,15 @@
 package com.binary.app.auth
 
 import com.binary.app.auth.dto.UserCredDto
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("auth")
 class AuthController(private val authService: AuthService)  {
     @PostMapping
-    fun auth(@RequestBody userCred: UserCredDto) = authService.authenticate(userCred)
+    fun auth(@RequestBody userCred: UserCredDto) =
+        if (authService.authenticate(userCred)) ResponseEntity(true, HttpStatus.OK)
+        else ResponseEntity(false, HttpStatus.UNAUTHORIZED)
 }
